c29aa02173e9f8b763a17a7f5528b4712e52293d
Şeyma Yıldırım <seymayldrm677@gmail.com>
2021-01-19 19:50:26 +0300

    ClassMetotDemo added

---
+++ ClassMetotDemo/ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.musteriNo +"  "+ musteri.Ad + "  " + musteri.Soyad + " Kullanıcının Ekleme İşlemi Yapıldı.");
+        }
+
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.musteriNo + "  " + musteri.Ad + "  " + musteri.Soyad + " Kullanıcının Silme İşlemi Yapıldı.");
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.musteriNo + "  " + musteri.Ad + "  " + musteri.Soyad + " Listeleme İşlemi Yapıldı.");
+        }
+    }
+}
---
+++ ClassMetotDemo/ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+
+            musteri1.Ad = "Seyma";
+            musteri1.Soyad = "Yıldırım";
+            musteri1.musteriNo = 121530;
+
+            Musteri musteri2 = new Musteri();
+
+            musteri2.Ad = "Esma";
+            musteri2.Soyad = "Kaya";
+            musteri2.musteriNo = 506397;
+
+            Musteri[] musteriler = new Musteri[]
+            {
+                musteri1,musteri2
+            };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Silme(musteri2);
+            musteriManager.Listele(musteri1);
+        }
+    }
+}
